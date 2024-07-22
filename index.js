9ac0f9720a1e9201ae07a37ab17632ff60e1e8e5
sudo-pawan <mailpawanpoudel@gmail.com>
2024-07-22 16:35:10 +0545

    Update index.js

--- index.js
+++ index.js
@@ -19,14 +19,18 @@ io.on("connection", (socket) => {
     const room = `${group}-${page}`;
     socket.join(room);
     
+    // Notify other users in the room that a new user has joined
     socket.broadcast.to(room).emit("userJoined", user);
 
     socket.on("disconnect", () => {
+      console.log("user disconnected:", socket.id);
+      // Notify other users in the room that the user has left
       socket.broadcast.to(room).emit("userLeft", user);
     });
 
     socket.on("pageChange", (data) => {
-      socket.to(room).emit("pageChange", data);
+      // Broadcast to all other clients in the room except the sender
+      socket.broadcast.to(room).emit("pageChange", data);
     });
   });
 });
